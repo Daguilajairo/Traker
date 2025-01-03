8604b593d5ca096cf68080e94ae4912b82c03a76
Jairo D'aguila <daguilajairo@gmail.com>
2025-01-03 15:28:09 -0300

    a

---
+++ src/components/BoxComponent.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="box has-text-weigth-bold">
+<slot></slot>
+ </div>
+</template>
+
+<script lang="ts">
+import { defineComponent} from 'vue';
+
+export default defineComponent({
+    name:"BoxComponent"
+})
+</script>
+
+<style scoped>
+.box{
+    background:#6c6c6c ;
+}
+</style>
